89a9ce320bd48f3160dba5bc0d20d9c9511097bc
unknown <mdurks@gmail.com>
2018-02-05 22:14:00 +0000

    Boilerplate Init

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+
+
+
+//  ! Important
+
+//  Place the minified version of this file into the script tag
+//  at the bottom of the html file this is used on.
+//  This is for pagespeed purposes.
+
+
+function KeyPress(n) { var t = window.event ? event : n; t.keyCode == 90 && t.shiftKey && t.altKey && $("html").toggleClass("outlineHelper") }
+document.onkeydown = KeyPress;
+
+
+$(document).ready(function()
+{
+
+    //$('html').addClass('outlineHelper');
+
+    $('.js-page-welcome-btn').on('click', function(event){
+        event.preventDefault();
+        $('.js-page-welcome').hide();
+        $('.js-page-page2').show();
+    });
+
+
+});
